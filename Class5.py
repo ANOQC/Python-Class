24fa7c5dd4346086d4068bf76adb3ec53e1b6e07
ANO_QC <150508313+ANOQC@users.noreply.github.com>
2023-11-27 21:59:28 +0530

    Add files via upload

---
+++ Class5.py
@@ -0,0 +1,4 @@
+first_name = "One"
+last_name = "Piece"
+full_name = f"{first_name} {last_name}"
+print(full_name)
---
+++ Class6.py
@@ -0,0 +1,9 @@
+# Addition
+print(2 + 2)
+# Subtraction
+print(15 - 10)
+# Multiplication
+print(2 * 2)
+# Division
+print( 10 / 5)
+
